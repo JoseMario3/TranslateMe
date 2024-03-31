dc5f17649f6d394f2ecae775648de36023d2b134
Jose Folgar <f.jose@wustl.edu>
2024-03-31 18:49:28 -0500

    Added Firebase

---
+++ TranslateMe/TranslateMe/TranslateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateMeApp.swift
+//  TranslateMe
+//
+//  Created by Jose Folgar on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
