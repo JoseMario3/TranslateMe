34e489b3f16c26bdd8131ef02c64fdbcbc3cd65c
Jose Folgar <f.jose@wustl.edu>
2024-04-07 23:40:14 -0500

    Assignment done enough

--- TranslateMe/TranslateMe/ContentView.swift
+++ TranslateMe/TranslateMe/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var word: String = ""
+    @State private var translation: String = ""
+    @State var translationManager: TranslationManager
+    
+    init(isMocked: Bool = false) {
+        translationManager = TranslationManager(isMocked: isMocked)
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("Translate Me")
+                TextField("Word", text: $word)
+                Button("Translate!") {
+                    Task {
+                        await getTranslation()
+                    }
+                }
+                Text("\(translation)")
+                NavigationLink("See Previous") {
+                    RecordsView(translations: translationManager.translations)
+                }
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    private func getTranslation() async {
+        let url = URL(string: "https://api.mymemory.translated.net/get?q=\(word.lowercased())&langpair=en-us|es-mx")!
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let response = try JSONDecoder().decode(TranslatedResponse.self, from: data)
+            self.translation = response.responseData.translatedText
+            translationManager.sendTranslation(word: self.word, translation: self.translation)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
 
